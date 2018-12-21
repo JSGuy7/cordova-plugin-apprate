06f6cf24b9d393bcc534e8cd302c2eb96f5f671c
sushchevskiy <anton.sushchevskiy@gmail.com>
2018-12-21 23:57:38 +0200

    open rate dialog immediately

--- www/AppRate.js
+++ www/AppRate.js
@@ -149,7 +149,7 @@ AppRate = (function() {
       localeObj = Locales.getLocale(AppRate.preferences.useLanguage, AppRate.preferences.displayAppName, AppRate.preferences.customLocale);
 
       if(AppRate.preferences.simpleMode) {
-      	promptForStoreRatingWindowButtonClickHandler();
+      	promptForStoreRatingWindowButtonClickHandler(3);
         // navigator.notification.confirm(localeObj.message, promptForStoreRatingWindowButtonClickHandler, localeObj.title, [localeObj.cancelButtonLabel, localeObj.laterButtonLabel, localeObj.rateButtonLabel]);
       } else {
         navigator.notification.confirm(localeObj.appRatePromptMessage, promptForAppRatingWindowButtonClickHandler, localeObj.appRatePromptTitle, [localeObj.noButtonLabel, localeObj.yesButtonLabel]);
